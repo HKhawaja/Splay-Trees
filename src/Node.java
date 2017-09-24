aec3e670421d1abd9fe12cc9bed2e2cde872bd07
Harith Khawaja <mkhawaja19@amherst.edu>
2017-09-24 19:12:10 +0200

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,44 +1,43 @@
-public class Node
-{
-public Node parent;
-public Node left;
-public Node right;
+public class Node {
+	public Node parent;
+	public Node left;
+	public Node right;
 
-public int start;
-public int end;
+	public int start;
+	public int end;
 
-public Node(int start, int end, Node p)
-    {
-	this.start = start;
-	this.end = end;
-	parent = p;
-    }
+	public Node(int start, int end, Node p)
+	    {
+		this.start = start;
+		this.end = end;
+		parent = p;
+	    }
 
-public boolean isLeftChild()
-{
-    return (parent != null && parent.left == this);
-}
+	public boolean isLeftChild()
+	{
+	    return (parent != null && parent.left == this);
+	}
 
-public boolean isRightChild()
-{
-    return (parent != null && parent.right == this);
-}
+	public boolean isRightChild()
+	{
+	    return (parent != null && parent.right == this);
+	}
 
-public int compareTo (int n) {
-	if (start > n)
-		return 1;
-	if (n > end)
-		return -1;
-	return 0;	
-}
+	public int compareTo (int n) {
+		if (start > n)
+			return 1;
+		if (n > end)
+			return -1;
+		return 0;	
+	}
 
-public void iterate() {
-	if (left!= null)
-		left.iterate();
-	if (this.parent == null)
-		System.out.print("*");
-	System.out.print("<" + this.start + "-" + this.end + "> ");
-	if (right!= null)
-		right.iterate();
-}
+	public void iterate() {
+		if (left!= null)
+			left.iterate();
+		if (this.parent == null)
+			System.out.print("*");
+		System.out.print("<" + this.start + "-" + this.end + "> ");
+		if (right!= null)
+			right.iterate();
+	}
 }
